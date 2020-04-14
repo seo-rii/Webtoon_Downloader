4ae87d296b81390eff042d9e6127e434a93c234f
Seo_Hyun <04seohyun@gmail.com>
2020-04-14 11:03:55 +0900

    Support for Alpha Channel Image(Cuttoon, ...)

--- module/merge.py
+++ module/merge.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 from PIL import Image
 from img2pdf import convert
 
@@ -33,9 +34,41 @@ def mergeImage(op, webtoonId, viewNo, cutNo, savePath, tmpPath, runningThreadNo,
     runningThreadNo.value -= 1
 
 
+def alpha_composite(front, back):
+    """Alpha composite two RGBA images.
+    Source: http://stackoverflow.com/a/9166671/284318
+    Keyword Arguments:
+    front -- PIL RGBA Image object
+    back -- PIL RGBA Image object
+    """
+    front = np.asarray(front)
+    back = np.asarray(back)
+    result = np.empty(front.shape, dtype='float')
+    alpha = np.index_exp[:, :, 3:]
+    rgb = np.index_exp[:, :, :3]
+    falpha = front[alpha] / 255.0
+    balpha = back[alpha] / 255.0
+    result[alpha] = falpha + balpha * (1 - falpha)
+    old_setting = np.seterr(invalid='ignore')
+    result[rgb] = (front[rgb] * falpha + back[rgb] * balpha * (1 - falpha)) / result[alpha]
+    np.seterr(**old_setting)
+    result[alpha] *= 255
+    np.clip(result, 0, 255)
+    result = result.astype('uint8')
+    result = Image.fromarray(result, 'RGBA')
+    return result
+
+
 def mergeImagePdf(op, webtoonId, viewNo, cutNo, savePath, tmpPath, runningThreadNo, cookie):
     pdf_list = []
     for i in range(0, cutNo):
+        im = Image.open(
+            os.path.join(tmpPath, getWebtoonName(op, webtoonId, cookie) + "_" + str(viewNo) + "_" + str(i) + ".png"))
+        if len(im.getbands()) == 4:
+            back = Image.new('RGBA', size=im.size, color=(255, 255, 255) + (255,))
+            im = alpha_composite(im, back).convert('RGB')
+            im.save(os.path.join(tmpPath,
+                                 getWebtoonName(op, webtoonId, cookie) + "_" + str(viewNo) + "_" + str(i) + ".png"))
         pdf_list.append(
             os.path.join(tmpPath, getWebtoonName(op, webtoonId, cookie) + "_" + str(viewNo) + "_" + str(i) + ".png"))
     pdf = convert(pdf_list)
